fe3bf02ed9b8219537a53f8f0838d5eaa0e5784d
SampurnaC <sampurna.chapagainnn@gmail.com>
2016-10-05 08:59:09 -0700

    comment has been added with the delete button !

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum:5 }
 	validates :body, presence: true
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }
